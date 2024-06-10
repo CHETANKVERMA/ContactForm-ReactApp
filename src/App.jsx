38b2dbb3a1bd7ae29d8440f34c96d61f2d973fa3
CHETANKVERMA <chetanverms201@gmail.com>
2024-06-10 08:24:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-
-import React from 'react'
-import './App.css'
-import Navbar from "../src/components/Navigation/Navbar"
-import ContactHeader from './components/ContactBody/ContactHeader'
-import FormInBody from './components/FormInBody/FormInBody'
+import React from "react";
+import "./App.css";
+import Navbar from "../src/components/Navigation/Navbar";
+import ContactHeader from "./components/ContactBody/ContactHeader";
+import FormInBody from "./components/FormInBody/FormInBody";
 
 function App() {
- 
-
   return (
     <>
-     <Navbar/>
-     <ContactHeader/>
-     <FormInBody/>
+      <Navbar />
+      <ContactHeader />
+      <FormInBody />
     </>
-       
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FormInBody/FormInBody.jsx
+++ src/components/FormInBody/FormInBody.jsx
@@ -7,81 +7,77 @@ import { MdEmail } from "react-icons/md";
 import { useState } from "react";
 
 const FormInBody = () => {
-  
   // const[intialName,setInName]= useState("")
   // const[intialEmail,setInEmail]= useState("")
   // const[intialText,setInText]= useState("")
 
-  
-  const [name,setName] = useState("TATA 1mg");
-  const [email,setEmail] = useState("care@1mg.com");
-  const [text,setText] = useState("Your health our responsibility");
+  const [name, setName] = useState("TATA 1mg");
+  const [email, setEmail] = useState("care@1mg.com");
+  const [text, setText] = useState("Your health our responsibility");
 
- 
+  const Submitting = (e) => {
+    e.preventDefault();
+    setName(e.target[0].value);
+    setEmail(e.target[1].value);
+    setText(e.target[2].value);
 
-
-
-  const Submitting = (e) =>{
-  e.preventDefault();
-  setName(e.target[0].value);
-  setEmail(e.target[1].value);
-  setText(e.target[2].value);
-
-  // setInEmail("");
-  // setInName("");
-  // setInText("");
-  }
+    // setInEmail("");
+    // setInName("");
+    // setInText("");
+  };
 
   return (
     <div className={styles.container}>
-     
       <div className={styles.form}>
-       
         <div className={styles.topButton}>
           {/* In icon we have kept tags intact because those are components that we imported */}
-          <Buttons text="VIA CHAT SUPPORT " icon={<SiGooglemessages />}  />
+          <Buttons text="VIA CHAT SUPPORT " icon={<SiGooglemessages />} />
           <Buttons text="VIA CALL " icon={<BsFillTelephoneFill />} />
         </div>
 
         <div className={styles.emailButton}>
-          <Buttons text="VIA EMAIL" icon={<MdEmail />} conditionForButton={true}/>
+          <Buttons
+            text="VIA EMAIL"
+            icon={<MdEmail />}
+            conditionForButton={true}
+          />
         </div>
 
-        <form onSubmit={Submitting} >
-         
-        <div className={styles.box}>
-        <label htmlFor={styles.yourName}>Name</label>
-        <input type='text' id ='yourName' name="nameBox" className={styles.inputsInsideForm}/>
-        </div>
+        <form onSubmit={Submitting}>
+          <div className={styles.box}>
+            <label htmlFor={styles.yourName}>Name</label>
+            <input
+              type="text"
+              id="yourName"
+              name="nameBox"
+              className={styles.inputsInsideForm}
+            />
+          </div>
 
-        <div className={styles.box}>
-        <label htmlFor={styles.yourEmail}>Email</label>
-        <input type='email' id ='youremail' name="emailBox" className={styles.inputsInsideForm}/>
-        </div>
+          <div className={styles.box}>
+            <label htmlFor={styles.yourEmail}>Email</label>
+            <input
+              type="email"
+              id="youremail"
+              name="emailBox"
+              className={styles.inputsInsideForm}
+            />
+          </div>
 
-        <div className={styles.box}>
-        <label htmlFor={styles.yourText}>Query</label>
-        <textarea type='text' id ='yourName' name="textBox"/>
-        </div>
+          <div className={styles.box}>
+            <label htmlFor={styles.yourText}>Query</label>
+            <textarea type="text" id="yourName" name="textBox" />
+          </div>
 
-        
-        <div className={styles.submitButton}>
-        <Buttons text="SUBMIT " />
-        </div>
-        
-      </form>
-
-      <div className={styles.lastTagofForm}>
-        {name +" | " + email +" | " + text}
+          <div className={styles.submitButton}>
+            <Buttons text="SUBMIT " />
+          </div>
+        </form>
       </div>
-
-      </div>
-{/* ************If image is inside public folder it can also be inserted like this*********** */}
+      {/* ************If image is inside public folder it can also be inserted like this*********** */}
       <div className="imgInForm">
-        <img src="iconBody.png"/>
+        <img className="imgc" src="iconBody.jpg" height="500" />
       </div>
-
-     
     </div>
   );
 };
